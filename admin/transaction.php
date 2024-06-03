db1a117ab5fc1622c504727a97338cbae0998b1a
meri-hane <janecalulang@gmail.com>
2024-06-03 10:43:42 +0800

    fixed logos and profile in book

--- admin/transaction.php
+++ admin/transaction.php
@@ -288,12 +288,7 @@ $(document).ready(function(){
 </div>
         </main>
 
-    <!-- ======= Footer ======= -->
-  <footer id="footer" class="footer">
-    <div class="copyright">
-      &copy; Copyright <strong><span>Mary Jane P. Calulang</span></strong>. All Rights Reserved
-    </div>
-  </footer><!-- End Footer -->
+  <?php include 'includes/footer.php'; ?>
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
